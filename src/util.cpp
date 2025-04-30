5639aeae943a3331a59ad40451ce1bca70b21e71
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-04-30 17:42:05 +0200

    win

--- src/util.cpp
+++ src/util.cpp
@@ -1053,14 +1053,30 @@ void PrintExceptionContinue(std::exception* pex, const char* pszThread)
 boost::filesystem::path GetDefaultDataDir()
 {
     namespace fs = boost::filesystem;
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Peercoin
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Peercoin
+    // Mac: ~/Library/Application Support/Peercoin
+    // Unix: ~/.peercoin
+#ifdef WIN32
+    // Windows
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Fastpeercoin";
+#else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
     if (pszHome == NULL || strlen(pszHome) == 0)
         pathRet = fs::path("/");
     else
         pathRet = fs::path(pszHome);
-
+#ifdef MAC_OSX
+    // Mac
+    pathRet /= "Library/Application Support";
+    fs::create_directory(pathRet);
+    return pathRet / "Fastpeercoin";
+#else
+    // Unix
     return pathRet / ".fastpeercoin";
+#endif
+#endif
 }
 
 boost::filesystem::path GetOldDefaultDataDir()
