ed08112affd5d8bbd9b5b35249604666e1c4002a
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-05-01 17:39:21 +0200

    depends

---
+++ depends/packages/bdb.mk
@@ -0,0 +1,31 @@
+package=bdb
+$(package)_version=5.3.28
+$(package)_download_path=https://download.oracle.com/berkeley-db
+$(package)_file_name=db-$($(package)_version).tar.gz
+$(package)_sha256_hash=e0a992d740709892e81f9d93f06daf305cf73fb81b545afe72478043172c3628
+$(package)_build_subdir=build_unix
+
+define $(package)_set_vars
+$(package)_config_opts=--disable-shared --enable-cxx --disable-replication
+$(package)_config_opts_mingw32=--enable-mingw
+$(package)_config_opts_linux=--with-pic
+endef
+
+define $(package)_preprocess_cmds
+  sed -i.old 's/WinIoCtl.h/winioctl.h/g' src/dbinc/win_db.h && \
+  sed -i.old 's/__atomic_compare_exchange\\(/__atomic_compare_exchange_db(/' src/dbinc/atomic.h && \
+  sed -i.old 's/atomic_init/atomic_init_db/' src/dbinc/atomic.h src/mp/mp_region.c src/mp/mp_mvcc.c src/mp/mp_fget.c src/mutex/mut_method.c src/mutex/mut_tas.c && \
+  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub dist
+endef
+
+define $(package)_config_cmds
+  ../dist/$($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE) libdb_cxx-5.3.a libdb-5.3.a
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install_lib install_include
+endef
